91d7491b8a8e2619db611455afce5fe92aac4ccf
andreyyarchuk <dron4432@gmail.com>
2023-12-11 23:11:38 +0200

    2.2.2: start programming table in database

--- db.js
+++ db.js
@@ -5,9 +5,15 @@ const db = new sqlite3.Database(dbName)
 db.serialize( ()=> {
     const sql = `
     CREATE TABLE IF NOT EXISTS articles
-    (id integer primary key, title, content TEXT)
+    (id integer primary key,
+        author TEXT,
+        title TEXT,
+        content TEXT,
+        picture TEXT)
     `
     db.run(sql)
 })
+
+
 
 module.exports = db
