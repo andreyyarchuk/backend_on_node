99f04f45a0291b1c5cb726ae12b68327d7c7b8d7
andreyyarchuk <dron4432@gmail.com>
2023-12-11 22:09:06 +0200

    2.1.0: add command processing. now express can convert .json

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const port = 5000
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     console.log(req.query)
     res.status(200).json('hello word')
 })
 
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.status(200).json('hello word')
+})
 app.listen(port, () => {
     console.log('server loading http://localhost:5000')
 })
