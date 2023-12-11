7d4f01e08161b0a35e1d63e45372cbb51b21e8e7
andreyyarchuk <dron4432@gmail.com>
2023-12-11 21:40:57 +0200

    2.0.1: server on express

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("server working")
+const express = require('express')
+// import express from 'express'
+
+const port = 5000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log(req.query)
+    res.status(200).json('hello word')
+})
+
+app.listen(port, () => {
+    console.log('server loading http://localhost:5000')
+})
