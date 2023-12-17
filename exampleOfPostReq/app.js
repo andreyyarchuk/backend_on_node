542c8c64297cd2c802f86863d3bc9b7c0caff0fb
andreyyarchuk <dron4432@gmail.com>
2023-12-17 22:39:35 +0200

    2.2.2.1.1.3.1 example_db: add INSERT to bd, but it does not work correct

--- exampleOfPostReq/app.js
+++ exampleOfPostReq/app.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser')
 const res = require('express/lib/response')
 const app = express()
 const sqlite = require('sqlite3').verbose()
+
+let sql
+
 const db = new sqlite.Database('./quote.db', sqlite.OPEN_READWRITE, (err) => {
     if (err) return console.error(err)
 })
@@ -14,7 +17,13 @@ app.use(bodyParser.json())
 
 app.post('/quote', (req, res)  => {
     try {
-        console.log(req.body.movie)
+        const {movie, quote, character} = req.body
+        sql = `INSERT INTO quote (movie, quote, character) VALUES (?,?,?)`
+        db.run(sql, [movie, quote, character], (err) => {
+            if (err) return res.json({status: 300, success: false, error: err})
+        
+        console.log('successful input', movie, quote, character)
+        })
         return res.json({
             status: 200,
             success: true,
