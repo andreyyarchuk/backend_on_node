cdc43e9c0255962371191add20e2bdc72126ac95
andreyyarchuk <dron4432@gmail.com>
2023-12-17 22:15:25 +0200

    2.2.2.1.1.3 example db: fix code. Now post req.body.movie from POSTMAN

--- exampleOfPostReq/app.js
+++ exampleOfPostReq/app.js
@@ -5,8 +5,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const res = require('express/lib/response')
 const app = express()
-
-
 const sqlite = require('sqlite3').verbose()
 const db = new sqlite.Database('./quote.db', sqlite.OPEN_READWRITE, (err) => {
     if (err) return console.error(err)
@@ -14,7 +12,7 @@ const db = new sqlite.Database('./quote.db', sqlite.OPEN_READWRITE, (err) => {
 
 app.use(bodyParser.json())
 
-app.post('./quote.db', (req, res)  => {
+app.post('/quote', (req, res)  => {
     try {
         console.log(req.body.movie)
         return res.json({
--- exampleOfPostReq/table.js
+++ exampleOfPostReq/table.js
@@ -3,5 +3,5 @@ const db = new sqlite.Database('./quote.db', sqlite.OPEN_READWRITE, (err) => {
     if (err) return console.error(err)
 })
 
-const sql = `CREATE TABLE   qoute(ID INTEGER PRIMARY KEY, movie, character)`
+const sql = `CREATE TABLE quote(ID INTEGER PRIMARY KEY, movie, quote, character)`
 db.run(sql)
