d39a7b1540a75c1682190c9b74aeb272fa684cbe
andreyyarchuk <dron4432@gmail.com>
2024-01-30 22:36:16 +0200

    2.4.0.6.1 exampleAuthentication: fix post-method for login

--- exampleAuthentication/app.js
+++ exampleAuthentication/app.js
@@ -77,53 +77,53 @@ const token = jwt.sign(
 )
 
 // Code for the Login Action
-app.post("/Users", async (req, res) => {
-    try {
-        const {Email, Password} = req.body
-        // Make sure there is an Email and Password in the request 
-        if (!(Email && Password)) {
-            res.status(400).send("All input is required")
-        }
-
-        let user = []
-
-        var sql = 'SELECT * FROM User WHERE Email = ?'
-        db.all(sql, Email, function(err, rows) {
-            if (err) {
-                res.status(400).json({"error": err.message})
-                return
-            }
-
-        rows.forEach(function (row) {
-            user.push(row)
-        })
-
-        var PHush = bcrypt.hash.hashSync(Password)
-
-        if (PHush === user[0].Password) {
-            // * CREATE JWT TOKEN
-
-            const token = jwt.sign(
-                {user_id: user[0].Id, username: user[0].Username, Email},
-                    process.env.TOLEN_KEY,
-                    {
+app.post("/api/login", async (req, res) => {
+  
+    try {      
+      const { Email, Password } = req.body;
+          // Make sure there is an Email and Password in the request
+          if (!(Email && Password)) {
+              res.status(400).send("All input is required");
+          }
+              
+          let user = [];
+          
+          var sql = "SELECT * FROM Users WHERE Email = ?";
+          db.all(sql, Email, function(err, rows) {
+              if (err){
+                  res.status(400).json({"error": err.message})
+                  return;
+              }
+  
+              rows.forEach(function (row) {
+                  user.push(row);                
+              })
+              
+              var PHash = bcrypt.hashSync(Password, user[0].Salt);
+         
+              if(PHash === user[0].Password) {
+                  // * CREATE JWT TOKEN
+                  const token = jwt.sign(
+                      { user_id: user[0].Id, username: user[0].Username, Email },
+                        process.env.TOKEN_KEY,
+                      {
                         expiresIn: "1h", // 60s = 60 seconds - (60m = 60 minutes, 2h = 2 hours, 2d = 2 days)
-                    }
-            )
-
-            user[0].Token = token
-        
-        } else {
-            return res.status(400).send('No Match')
-        }
-
-    return res.status(200).send(user)
-        })
-
-    } catch (error) {
-        console.log(error)
-    }
-})
+                      }  
+                  );
+  
+                  user[0].Token = token;
+  
+              } else {
+                  return res.status(400).send("No Match");          
+              }
+  
+             return res.status(200).send(user);                
+          });	
+      
+      } catch (err) {
+        console.log(err);
+      }    
+  });
 
 function startApp() {
     try {
