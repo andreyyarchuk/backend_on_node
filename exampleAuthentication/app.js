9df08a02eb604656ea737abf475a8003fc2ea98f
andreyyarchuk <dron4432@gmail.com>
2024-01-30 22:38:50 +0200

    2.4.0.7 exampleAuthentication: add post-methods TEST, REGISTRATION

--- exampleAuthentication/app.js
+++ exampleAuthentication/app.js
@@ -76,6 +76,75 @@ const token = jwt.sign(
     }
 )
 
+
+app.post("/api/register", async (req, res) => {
+    var errors=[]
+    try {
+        const { Username, Email, Password } = req.body;
+
+        if (!Username){
+            errors.push("Username is missing");
+        }
+        if (!Email){
+            errors.push("Email is missing");
+        }
+        if (errors.length){
+            res.status(400).json({"error":errors.join(",")});
+            return;
+        }
+        let userExists = false;
+        
+        
+        var sql = "SELECT * FROM Users WHERE Email = ?"        
+        await db.all(sql, Email, (err, result) => {
+            if (err) {
+                res.status(402).json({"error":err.message});
+                return;
+            }
+            
+            if(result.length === 0) {                
+                
+                var salt = bcrypt.genSaltSync(10);
+
+                var data = {
+                    Username: Username,
+                    Email: Email,
+                    Password: bcrypt.hashSync(Password, salt),
+                    Salt: salt,
+                    DateCreated: Date('now')
+                }
+        
+                var sql ='INSERT INTO Users (Username, Email, Password, Salt, DateCreated) VALUES (?,?,?,?,?)'
+                var params =[data.Username, data.Email, data.Password, data.Salt, Date('now')]
+                var user = db.run(sql, params, function (err, innerResult) {
+                    if (err){
+                        res.status(400).json({"error": err.message})
+                        return;
+                    }
+                  
+                });           
+            }            
+            else {
+                userExists = true;
+                // res.status(404).send("User Already Exist. Please Login");  
+            }
+        });
+  
+        setTimeout(() => {
+            if(!userExists) {
+                res.status(201).json("Success");    
+            } else {
+                res.status(201).json("Record already exists. Please login");    
+            }            
+        }, 500);
+
+
+    } catch (err) {
+      console.log(err);
+    }
+})
+
+
 // Code for the Login Action
 app.post("/api/login", async (req, res) => {
   
@@ -124,6 +193,12 @@ app.post("/api/login", async (req, res) => {
         console.log(err);
       }    
   });
+
+// * T E S T  
+
+app.post("/api/test", auth, (req, res) => {
+    res.status(200).send("Token Works - Yay!");
+});
 
 function startApp() {
     try {
